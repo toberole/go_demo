8330508498756ea33a790e4a6ad265ce6ce801b3
zhouwei441300 <zhouwei441300@163.com>
2018-12-23 16:12:59 +0800

    add

--- src/hello.go
+++ src/hello.go
@@ -11,7 +11,8 @@ func hello(w http.ResponseWriter, r *http.Request) {
 	io.WriteString(w, "Hello world!")
 }
 
-func get_param(w http.ResponseWriter,r *http.Request)  {
+// 获取get 参数
+func get_param(w http.ResponseWriter, r *http.Request) {
 	fmt.Println("===========get_param==========")
 
 	query := r.URL.Query()
@@ -19,6 +20,18 @@ func get_param(w http.ResponseWriter,r *http.Request)  {
 	fmt.Println(name)
 
 	w.Write([]byte("hello get"))
+}
+
+// 获取post方法的不带文件的参数
+func post_param(w http.ResponseWriter, r *http.Request) {
+	fmt.Println("=============post_param==============")
+
+	name := r.PostFormValue("name")
+	pwd := r.PostFormValue("pwd")
+
+	fmt.Printf("name: %s, pwd: %s",name,pwd)
+
+	w.Write([]byte(" hello post_param"))
 }
 
 func main() {
@@ -30,6 +43,7 @@ func main() {
 	// 注册一个处理函数
 	mux.HandleFunc("/hello", hello)
 	mux.HandleFunc("/get_param", get_param)
+	mux.HandleFunc("/post_param", post_param)
 
 	// 启动服务 监听8000端口
 	http.ListenAndServe(":8000", mux)
