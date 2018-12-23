8f27c002912bd1381e467d74bdc0a9cb4f99e661
zhouwei441300 <zhouwei441300@163.com>
2018-12-23 13:15:30 +0800

    demo

--- src/hello.go
+++ src/hello.go
@@ -6,15 +6,31 @@ import (
 	"net/http"
 )
 
+// 按照规定的签名定义 作为http的处理函数
 func hello(w http.ResponseWriter, r *http.Request) {
-	println(r.Body)
 	io.WriteString(w, "Hello world!")
 }
 
-func main() {
+func get_param(w http.ResponseWriter,r *http.Request)  {
+	fmt.Println("===========get_param==========")
 
-	fmt.Printf("hello 你好世界")
+	query := r.URL.Query()
+	name := query["name"][0]
+	fmt.Println(name)
+
+	w.Write([]byte("hello get"))
+}
+
+func main() {
+	fmt.Println("hello 你好世界")
+
+	// 路由 可以使用DefaultServeMux 默认的
 	mux := http.NewServeMux()
-	mux.HandleFunc("/", hello)
+
+	// 注册一个处理函数
+	mux.HandleFunc("/hello", hello)
+	mux.HandleFunc("/get_param", get_param)
+
+	// 启动服务 监听8000端口
 	http.ListenAndServe(":8000", mux)
 }
